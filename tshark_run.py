73df09d98c2936f50ba633eb41a806481519fbf9
Matt Tierney <tierney@cs.nyu.edu>
2012-02-06 22:19:42 -0500

    Android and chrome setup.

--- tshark_run.py
+++ tshark_run.py
@@ -4,6 +4,7 @@ import gflags
 import subprocess
 import sys
 import time
+import threading
 from selenium import webdriver
 
 FLAGS = gflags.FLAGS
@@ -15,6 +16,38 @@ gflags.DEFINE_boolean('debug', False, 'produces debugging output')
 
 # gflags.RegisterValidator()
 
+_TIMEOUT = 30 # seconds.
+_IFACES = { 't-mobile' : 'usb0',
+            'verizon' : 'usb1' }
+
+_BROWSER = {
+  'chrome' : webdriver.Chrome(),
+  'firefox' : webdriver.Firefox(),
+  'android' : webdriver.Remote("http://localhost:8080/wd/hub",
+                               webdriver.DesiredCapabilities.ANDROID)}
+
+
+class Logger(object):
+  def __init__(self, carrier, browser, domain):
+    self.carrier = carrier
+    self.browser = browser
+    self.domain = domain
+
+  def run(self):
+    print 'Starting sniffer.'
+    pcap = subprocess.Popen(
+      ['tcpdump','-i','%s' % _IFACES[self.carrier],'-w',
+       '%s_%s_%s_%s.pcap' % (self.carrier, self.browser, self.domain,
+                             str(time.time()))])
+
+    print 'Starting browser.'
+    browser = _BROWSER[self.browser]
+    browser.get('http://' + self.domain) # Executes until "loaded."
+    browser.close()
+
+    pcap.terminate()
+
+
 def main(argv):
   try:
     argv = FLAGS(argv)  # parse flags
@@ -24,15 +57,8 @@ def main(argv):
   if FLAGS.debug:
     print 'non-flag arguments:', argv
 
-  print 'Starting tcpdump.'
-  tcpdump = subprocess.Popen('sudo tcpdump -i eth1 -evvn -w output.pcap', shell=True)
-  time.sleep(2)
-  print 'Starting firefox.'
-  browser = subprocess.Popen('firefox http://cnn.com', shell=True)
-  time.sleep(10)
-  print 'Killing everything..'
-  browser.kill()
-  tcpdump.kill()
+  l = Logger('t-mobile', 'chrome', 'cnn.com')
+  l.run()
 
 if __name__ == '__main__':
   main(sys.argv)
