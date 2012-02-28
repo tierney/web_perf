18946ced2afea5193be5c865fbd077f551999af7
Matt Tierney <tierney@cs.nyu.edu>
2012-02-28 12:48:22 -0500

    FssLogger ready.

--- src/FssLogger.py
+++ src/FssLogger.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
-import gflags
+
+import logging
 import subprocess
+import sys
 import threading
 import time
+
+logging.basicConfig(stream=sys.stdout)
+
+import gflags
 
 FLAGS = gflags.FLAGS
 
 gflags.DEFINE_string('sspath', None, 'Location of fss ss.', short_name = 's')
-gflags.DEFINE_string('filename', time.strftime('%Y_%m_%d_%H_%M_%S'),
+gflags.DEFINE_string('filename',
+                     time.strftime('%Y_%m_%d_%H_%M_%S') + '.ss.log',
                      'ss log file.', short_name = 'f')
 
 gflags.MarkFlagAsRequired('sspath')
 
-class FssLogger(object):
+class FssLogger(threading.Thread):
   def __init__(self, sspath, filename):
     self.sspath = sspath
     self.filename = filename
+    threading.Thread.__init__(self)
 
   def run(self):
     ss_log_fh = open(self.filename,'w')
     try:
-      ss_log = subprocess.Popen([ss.sspath,'g'], stdout = ss_log_fh)
-    except Exception, e:
-      print e
+      ss_log = subprocess.Popen([self.sspath,'-g'], stdout = ss_log_fh)
+      ss_log.wait()
+    except KeyboardInterrupt:
+      logging.info('Done.')
+      subprocess.call(['gzip', FLAGS.filename])
+    ss_log_fh.close()
 
 def main(argv):
   try:
@@ -32,6 +43,9 @@ def main(argv):
     sys.exit(1)
 
   fss = FssLogger(FLAGS.sspath, FLAGS.filename)
+  fss.run()
+
+
 
 if __name__=='__main__':
   main(sys.argv)
