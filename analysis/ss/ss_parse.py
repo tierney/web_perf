f34447f6757c50a0200bb7d038b9e13ae4b1f1c8
Matt Tierney <tierney@cs.nyu.edu>
2012-02-27 23:01:29 -0500

    ss business.,

--- analysis/ss/ss_parse.py
+++ analysis/ss/ss_parse.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
-import re
 import cPickle
+import logging
+import re
+import sys
+import gflags
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_string('filename', None, 'ss log file.', short_name = 's')
+gflags.DEFINE_string('picklename', 'conn_cwnd.pkl', 'Output pickle filename.',
+                     short_name = 'p')
+
+gflags.MarkFlagAsRequired('filename')
 
 USERS = re.compile('\(\(\"(.*)\",(\d+),(\d+)')
 
@@ -15,42 +28,54 @@ SEND = re.compile('send (\d+)([KMG]bps)')
 RCV_RTT = re.compile('rcv_rtt:(\d+)')
 RCV_SPACE = re.compile('rcv_space:(\d+)')
 
-with open('/tmp/time.log') as fh:
-  ss_log = [line.strip() for line in fh.readlines()]
-  ss_log.reverse()
+def main(argv):
+  try:
+    argv = FLAGS(argv)  # parse flags
+  except gflags.FlagsError, e:
+    logging.error('%s\nUsage: %s ARGS\n%s' % (e, sys.argv[0], FLAGS))
+    sys.exit(1)
 
-conn_cwnd = dict()
+  with open(FLAGS.filename) as fh:
+    ss_log = [line.strip() for line in fh.readlines()]
+    ss_log.reverse()
 
-while ss_log:
-  line = ss_log.pop()
+  conn_cwnd = dict()
 
-  if 'TIMESTAMP' in line:
-    try:
-      monotonic = ss_log.pop()
-      realtime = ss_log.pop()
-    except IndexError:
-      break
-    # print "New entry: %s . %s" % (monotonic, realtime)
+  while ss_log:
+    line = ss_log.pop()
 
-  ip_addrs = re.findall('[0-9]+(?:\.[0-9]+){3}:[0-9]+', line)
-  if ip_addrs:
-    src_ip_port, dest_ip_port = ip_addrs
-    tcp_4tuple = (src_ip_port, dest_ip_port)
-    continue
+    if 'TIMESTAMP' in line:
+      try:
+        monotonic = ss_log.pop()
+        realtime = ss_log.pop()
+      except IndexError:
+        break
+      # print "New entry: %s . %s" % (monotonic, realtime)
 
-  m = re.search(CWND, line)
-  if m:
-    cwnd = m.groups()[0]
-    if tcp_4tuple not in conn_cwnd:
-      conn_cwnd[tcp_4tuple] = []
-    ts_type, sec, nsec = realtime.split('\t')
-    conn_cwnd[tcp_4tuple].append((sec, nsec, cwnd))
+    ip_addrs = re.findall('[0-9]+(?:\.[0-9]+){3}:[0-9]+', line)
+    if ip_addrs:
+      src_ip_port, dest_ip_port = ip_addrs
+      tcp_4tuple = (src_ip_port, dest_ip_port)
+      continue
 
-print "Writing pickle."
+    m = re.search(CWND, line)
+    if m:
+      cwnd = m.groups()[0]
+      if tcp_4tuple not in conn_cwnd:
+        conn_cwnd[tcp_4tuple] = []
+      try:
+        sec, nsec = realtime.split('\t')
+      except ValueError, e:
+        logging.error('%s: %s.' % (str(e), realtime))
+        sys.exit(1)
+      conn_cwnd[tcp_4tuple].append((sec, nsec, cwnd))
 
-with open('conn_cwnd.pkl', 'w') as fh:
-  cPickle.dump(conn_cwnd, fh)
+  logging.info("Writing pickle.")
 
+  with open(FLAGS.picklename, 'w') as fh:
+    cPickle.dump(conn_cwnd, fh)
 
+if __name__=='__main__':
+  main(sys.argv)
 
 
--- src/run_iperf_reverse_server.py
+++ src/run_iperf_reverse_server.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
+import gflags
 import subprocess
+import sys
 import time
 
-def main():
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_string('iperf', None, 'iperf (reverse NAT-capable) path.',
+                     short_name = 'i')
+
+gflags.MarkFlagAsRequired('iperf')
+
+
+def main(argv):
+  try:
+    argv = FLAGS(argv)  # parse flags
+  except gflags.FlagsError, e:
+    logging.error('%s\nUsage: %s ARGS\n%s' % (e, sys.argv[0], FLAGS))
+    sys.exit(1)
+
   iperf_fh = open('%s_iperf.log' % str(time.time()), 'w')
   while True:
-    iperf_rs = subprocess.Popen(['iperf','-s','--reverse','-i','3','-t','45'],
-                                stdout=iperf_fh, stderr=iperf_fh)
+    iperf_rs = subprocess.Popen([FLAGS.iperf,'-s','--reverse','-i','3',
+                                 '-t','45'], stdout=iperf_fh, stderr=iperf_fh)
     iperf_rs.wait()
 
-main()
+if __name__=='__main__':
+  main(sys.argv)
 
