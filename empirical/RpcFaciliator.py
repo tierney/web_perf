fe3fcbfb03494b6f3a91ada690286fe1c2a84934
Matt Tierney <tierney@cs.nyu.edu>
2012-02-29 15:47:25 -0500

    RPC faciltating code more flexible.

--- empirical/RpcFaciliator.py
+++ empirical/RpcFaciliator.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
 import sys
-
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+
+import gflags
+
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_string('host', None, 'host address for XMLRPCServer',
+                     short_name = 'h')
+gflags.DEFINE_integer('port', None, 'port number for XMLRPCServer',
+                      short_name = 'p')
+gflags.MarkFlagAsRequired('host')
+gflags.MarkFlagAsRequired('port')
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
   rpc_paths = ('/RPC2',)
-
-# Create server
-server = SimpleXMLRPCServer(("thesues.news.cs.nyu.edu", 34343),
-                            requestHandler=RequestHandler)
-server.register_introspection_functions()
 
 class FileSignalWriter:
   def begin(self):
@@ -25,7 +30,22 @@ class FileSignalWriter:
     with open('HALT','w') as fh: pass
     sys.exit(0)
 
-server.register_instance(FileSignalWriter())
+def main(argv):
+  try:
+    argv = FLAGS(argv)  # parse flags
+  except gflags.FlagsError, e:
+    logging.error('%s\nUsage: %s ARGS\n%s' % (e, sys.argv[0], FLAGS))
+    sys.exit(1)
 
-# Run the server's main loop
-server.serve_forever()
+  # Create server
+  server = SimpleXMLRPCServer((FLAGS.host, FLAGS.port),
+                              requestHandler=RequestHandler)
+  server.register_introspection_functions()
+
+  server.register_instance(FileSignalWriter())
+
+  # Run the server's main loop
+  server.serve_forever()
+
+if __name__=='__main__':
+  main(sys.argv)
