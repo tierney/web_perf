d938f4a2f5910c11605cf2753fd436833e515f1f
Matt Tierney <tierney@cs.nyu.edu>
2012-03-12 17:02:49 -0400

    TimeoutServerProxy added.

--- empirical/run_ec2_rpc_client.py
+++ empirical/run_ec2_rpc_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import httplib
 import shlex
 import subprocess
 import sys
@@ -19,6 +20,31 @@ gflags.DEFINE_integer('rpcport', 34344, 'RPC port to connect to',
 
 gflags.MarkFlagAsRequired('ec2_region_hosts')
 
+class TimeoutHTTPConnection(httplib.HTTPConnection):
+   def connect(self):
+       httplib.HTTPConnection.connect(self)
+       self.sock.settimeout(self.timeout)
+
+class TimeoutHTTP(httplib.HTTP):
+   _connection_class = TimeoutHTTPConnection
+   def set_timeout(self, timeout):
+       self._conn.timeout = timeout
+
+class TimeoutTransport(xmlrpclib.Transport):
+    def __init__(self, timeout=10, *l, **kw):
+        xmlrpclib.Transport.__init__(self,*l,**kw)
+        self.timeout=timeout
+    def make_connection(self, host):
+        conn = TimeoutHTTP(host)
+        conn.set_timeout(self.timeout)
+        return conn
+
+class TimeoutServerProxy(xmlrpclib.ServerProxy):
+    def __init__(self,uri,timeout=10,*l,**kw):
+        kw['transport']=TimeoutTransport(timeout=timeout,
+                                         use_datetime=kw.get('use_datetime',0))
+        xmlrpclib.ServerProxy.__init__(self,uri,*l,**kw)
+
 
 def main(argv):
   try:
@@ -34,7 +60,8 @@ def main(argv):
     region, host = region_host.split(',')
 
     # Start server tcpdump
-    server = xmlrpclib.ServerProxy('http://%s:%d' % (host, FLAGS.rpcport))
+    server = TimeoutServerProxy('http://%s:%d' % (host, FLAGS.rpcport),
+                                timeout=5)
     uuid = str(uuid4())
     pid = server.start(region, uuid)
 
